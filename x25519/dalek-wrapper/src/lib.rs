a2c3e28471891ff4d587faabd0b3a7e955ef1f1c
David Wong <davidwong.crypto@gmail.com>
2021-06-11 13:39:24 -0700

    x25519: added missing functionalities

--- x25519/dalek-wrapper/src/lib.rs
+++ x25519/dalek-wrapper/src/lib.rs
@@ -1,7 +1,18 @@
 use rand::prelude::*;
+use std::convert::TryInto as _;
 use x25519_dalek as x25519;
 
-// create an ocaml type?
+// to delete
+extern "C" fn mytype_finalizer(_: ocaml::Raw) {
+    println!("This runs when the value gets garbage collected");
+}
+
+// [u8; 32] type (necessary for ocaml)
+
+#[derive(ocaml::IntoValue, ocaml::FromValue)]
+pub struct Array32Bytes(Vec<u8>);
+
+// private key stuff
 
 #[derive(Clone)]
 pub struct PrivateKey(x25519::StaticSecret);
@@ -13,24 +24,7 @@ unsafe impl<'a> ocaml::FromValue<'a> for PrivateKey {
     }
 }
 
-#[derive(Clone)]
-pub struct PublicKey(x25519::PublicKey);
-
-unsafe impl<'a> ocaml::FromValue<'a> for PublicKey {
-    fn from_value(value: ocaml::Value) -> Self {
-        let x: ocaml::Pointer<Self> = ocaml::FromValue::from_value(value);
-        x.as_ref().clone()
-    }
-}
-
-extern "C" fn mytype_finalizer(_: ocaml::Raw) {
-    println!("This runs when the value gets garbage collected");
-}
-
 ocaml::custom!(PrivateKey {
-    finalize: mytype_finalizer,
-});
-ocaml::custom!(PublicKey {
     finalize: mytype_finalizer,
 });
 
@@ -41,13 +35,10 @@ pub fn new() -> PrivateKey {
     PrivateKey(private_key)
 }
 
-#[derive(ocaml::IntoValue, ocaml::FromValue)]
-pub struct SharedSecret(Vec<u8>);
-
 #[ocaml::func]
-pub fn diffie_hellman(private_key: PrivateKey, public_key: PublicKey) -> SharedSecret {
+pub fn diffie_hellman(private_key: PrivateKey, public_key: PublicKey) -> Array32Bytes {
     let shared_secret = private_key.0.diffie_hellman(&public_key.0);
-    SharedSecret(shared_secret.to_bytes().into())
+    Array32Bytes(shared_secret.to_bytes().into())
 }
 
 #[ocaml::func]
@@ -56,4 +47,46 @@ pub fn to_public_key(private_key: PrivateKey) -> PublicKey {
     PublicKey(public_key)
 }
 
-//pub fn private_to_bytes(private_key: PrivateKey) ->
+#[ocaml::func]
+pub fn private_to_bytes(private_key: PrivateKey) -> Array32Bytes {
+    Array32Bytes(private_key.0.to_bytes().into())
+}
+
+#[ocaml::func]
+pub fn private_from_bytes(bytes: Array32Bytes) -> PrivateKey {
+    let private_bytes: [u8; 32] = bytes
+        .0
+        .try_into()
+        .expect("private key was not a 32-byte array");
+    PrivateKey(x25519::StaticSecret::from(private_bytes))
+}
+
+// public key stuff
+
+#[derive(Clone)]
+pub struct PublicKey(x25519::PublicKey);
+
+unsafe impl<'a> ocaml::FromValue<'a> for PublicKey {
+    fn from_value(value: ocaml::Value) -> Self {
+        let x: ocaml::Pointer<Self> = ocaml::FromValue::from_value(value);
+        x.as_ref().clone()
+    }
+}
+
+ocaml::custom!(PublicKey {
+    finalize: mytype_finalizer,
+});
+
+#[ocaml::func]
+pub fn public_to_bytes(public_key: PublicKey) -> Array32Bytes {
+    Array32Bytes(public_key.0.to_bytes().into())
+}
+
+#[ocaml::func]
+pub fn public_from_bytes(bytes: Array32Bytes) -> PublicKey {
+    let public_bytes: [u8; 32] = bytes
+        .0
+        .try_into()
+        .expect("public key was not a 32-byte array");
+    PublicKey(x25519::PublicKey::from(public_bytes))
+}
--- x25519/lib.ml
+++ x25519/lib.ml
@@ -1,9 +1,13 @@
 open Core_kernel
 
-type shared_secret = { inner : char array }
+type array32bytes = { inner : char array } [@@boxed]
 
 module PublicKey = struct
   type t
+
+  external to_bytes : t -> array32bytes = "public_to_bytes"
+
+  external of_bytes : array32bytes -> t = "public_from_bytes"
 end
 
 module PrivateKey = struct
@@ -13,9 +17,11 @@ module PrivateKey = struct
 
   external to_public_key : t -> PublicKey.t = "to_public_key"
 
-  (*  external to_bytes : t -> bytes = "to_bytes" *)
+  external to_bytes : t -> array32bytes = "private_to_bytes"
 
-  external diffie_hellman : t -> PublicKey.t -> shared_secret = "diffie_hellman"
+  external of_bytes : array32bytes -> t = "private_from_bytes"
+
+  external diffie_hellman : t -> PublicKey.t -> array32bytes = "diffie_hellman"
 end
 
 let%test_unit "two diffie-hellman, same result" =
@@ -23,4 +29,13 @@ let%test_unit "two diffie-hellman, same result" =
   let pub = PrivateKey.(generate () |> to_public_key) in
   let shared_secret = PrivateKey.diffie_hellman priv pub in
   let shared_secret2 = PrivateKey.diffie_hellman priv pub in
+  [%test_eq: char array] shared_secret.inner shared_secret2.inner
+
+let%test_unit "serialization of private key" =
+  let priv = PrivateKey.generate () in
+  let priv_bytes = PrivateKey.to_bytes priv in
+  let priv2 = PrivateKey.of_bytes priv_bytes in
+  let pub = PrivateKey.(generate () |> to_public_key) in
+  let shared_secret = PrivateKey.diffie_hellman priv pub in
+  let shared_secret2 = PrivateKey.diffie_hellman priv2 pub in
   [%test_eq: char array] shared_secret.inner shared_secret2.inner
